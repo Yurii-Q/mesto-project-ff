c0d046e7f38fb96b0c8531d53666a5fcf35416d6
Yurii Kozlov <k_yuuu@mail.ru>
2024-05-10 11:44:22 +0300

    Create index.js

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Yurii-Q/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTmp = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const mainPage = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard (cardData, deleteCard) {
+  const newCard = cardTmp.querySelector('.card').cloneNode('true');
+  const imgCard = newCard.querySelector('.card__image');
+  const titleCard = newCard.querySelector('.card__title');
+  const buttonDeleteCard = newCard.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  imgCard.src = cardData.link;
+  imgCard.alt = cardData.name;
+  titleCard.textContent = cardData.name;
+  buttonDeleteCard.addEventListener('click', deleteCard);
 
-// @todo: Вывести карточки на страницу
+  return newCard;
+}
+
+// Функция удаления карточки
+function deleteCard (evt) {
+  const itemList = evt.target.closest('.places__item');
+  itemList.remove();
+}
+
+// Вывести карточки на страницу
+initialCards.forEach((item) => {
+  const card = createCard(item, deleteCard);
+  mainPage.append(card);
+});
